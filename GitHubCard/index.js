f5843b7d6595d7b3e22a263f16e00da15be3c08d
Uros Ivanovic <urosivanovic@Uross-MBP-136.lan>
2021-12-02 14:02:32 -0500

    start

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,18 @@
+import axios from 'axios'
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+  axios.get('https://api.github.com/users/urosivanovich')
+  .then(response =>{
 
+  }).catch(error => {
+
+  }).finally(()=>{
+    console.log('doo doo')
+  })
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
